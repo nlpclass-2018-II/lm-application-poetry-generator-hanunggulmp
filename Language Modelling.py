d54588399497a47eb98406ae367ca0e45ce8b7b4
hanunggulmp <hanunggulmp@gmail.com>
2018-02-03 18:59:44 +0700

    Revised

--- Language Modelling.py
+++ Language Modelling.py
@@ -1,21 +1,65 @@
-import nltk
-from nltk import word_tokenize
-from nltk.util import ngrams
-from collections import Counter
+from nltk.collocations import *
+from operator import itemgetter
+from nltk import RegexpTokenizer
 
+tokenizer = RegexpTokenizer(r'(\w+)') #filter the coma, period, exclamation, etc
 input = open('Khairil.txt')
-output = open('Result Number 1 and 2.txt','w')
+output = open('Revised Number 1 and 2.txt','w')
 text = input.read()
-token = nltk.word_tokenize(text)
+token = tokenizer.tokenize(text.lower()) #get all text become lower case
+input.close()
 
-unigram = ngrams(token,1)
-bigrams = ngrams(token,2)
+#method to merge 2 poetries
+def mergePoem(tokens1,tokens2):
+    input = open(tokens2)
+    text = input.read()
+    tokens1 += tokenizer.tokenize(text.lower())
+    input.close()
+    return tokens1
 
-uniCounter = Counter(unigram)
-biCounter = Counter(bigrams)
+mergedTokens = mergePoem(token,'Sapardi.txt')
+print(mergedTokens)
+########################### UNIGRAM ##############################
+unigramCount = {}
 
-print("Unigram: ",str(uniCounter))
-print("Bigram : ",str(biCounter))
+#counting unigram words
+for word in mergedTokens:
+    if word in unigramCount:
+        unigramCount[word] += 1
+    else:
+        unigramCount[word] = 1
 
-output.write('Unigram: ' + str(uniCounter) + '\n' + '\n')
-output.write('Bigram : ' + str(biCounter))
+print(unigramCount)
+
+topTenUni = dict(sorted(unigramCount.items(),key=itemgetter(1), reverse=True)[:10])
+
+print(topTenUni)
+
+output.write('Unigram : ' + str(unigramCount) + '\n')
+output.write('Unigram Top 10: ' + str(topTenUni) + '\n' + '\n')
+
+######################################################################################
+########################### BIGRAM ##############################
+
+#Counting bigram words
+
+print('\n\n\n','BIGRAM')
+bigrams = {}
+finder = BigramCollocationFinder.from_words(mergedTokens, window_size = 2)
+n = list(finder.ngram_fd.items())
+bigrams = n.sort(key=lambda item: item[-1], reverse=True) #creator of bigram variable
+print(n)
+
+bigramCount = {}
+length = len(mergedTokens)
+for word in range(0,length - 1):
+    if mergedTokens[word] in bigramCount:
+        if mergedTokens[word+1] in bigramCount[mergedTokens[word]]:
+            bigramCount[mergedTokens[word]][mergedTokens[word + 1]] += 1
+        else:
+            bigramCount[mergedTokens[word]][mergedTokens[word + 1]] = 1
+    else:
+        bigramCount[mergedTokens[word]] = {}
+        bigramCount[mergedTokens[word]][mergedTokens[word + 1]] = 1
+
+output.write('Bigram : ' + str(n))
--- Perplexity.py
+++ Perplexity.py
@@ -1,17 +1,32 @@
-import nltk
 import collections, nltk
-from nltk.util import ngrams
-from nltk.corpus import brown
-from nltk.probability import LidstoneProbDist, WittenBellProbDist
+from nltk import RegexpTokenizer
 
-file = open('Khairil.txt')
+tokenizer = RegexpTokenizer(r'(\w+)') #filter the coma, period, exclamation, etc
+
+file = open('Poetry.txt')
 text = file.read()
-token = nltk.word_tokenize(text)
+testset = nltk.word_tokenize(text)
 
-file2 = open('Poetry.txt')
+file2 = open('Khairil.txt')
 text2 = file2.read()
-testset = nltk.word_tokenize(text2)
+train1 = nltk.word_tokenize(text2)
 
+file3 = open('Sapardi.txt')
+text3 = file3.read()
+train2 = nltk.word_tokenize(text3)
+
+#merge Khairil's and Sapardi's poem into a training data
+def mergePoem(train1,train2):
+    text2 = tokenizer.tokenize(train1.lower())
+    text3 = tokenizer.tokenize(train2.lower())
+    tokens = text2
+    tokens += text3
+    return tokens
+
+trainer = mergePoem(text2,text3)
+print(trainer)
+
+#compute the probability of each unigram's word
 def unigram(tokens):
     model = collections.defaultdict(lambda: 0.01)
     for f in tokens:
@@ -24,6 +39,7 @@ def unigram(tokens):
         model[word] = model[word]/float(sum(model.values()))
     return model
 
+#calculate the perplexity
 def perplexity(testset, model):
     perplexity = 1
     N = 0
@@ -34,8 +50,21 @@ def perplexity(testset, model):
     return perplexity
 
 
-model = unigram(token)
-print(perplexity(testset, model))
+model = unigram(trainer)#model for both poem
+model2 = unigram(train1)#model for Khairil's
+model3 = unigram(train2)#model for Sapardi's
 
-result = open('Perplexity Result.txt','w')
-result.write('The Perplexity of the Khairils poem and Poetry test is: '+ str(perplexity(testset, model)))
+
+print(model)
+print(model2)
+print(model3)
+
+#perplexity between datatest and datatraining)
+print('With both of 2 poems:',perplexity(testset, model))
+print('With Khairil\'s poem:',perplexity(testset,model2))
+print('With Sapardi\'s poem:',perplexity(testset,model3))
+
+result = open('[Revised] Perplexity Result.txt','w')
+result.write('With both of 2 poems:'+ str(perplexity(testset, model)))
+result.write('With Khairil\'s poem:'+ str(perplexity(testset,model2)))
+result.write('With Sapardi\'s poem:'+ str(perplexity(testset,model3)))
